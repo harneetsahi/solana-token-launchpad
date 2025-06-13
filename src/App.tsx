0a1480b5adc6478a814778ae7bff39692b066ec4
Harneet <harneetsahi8@gmail.com>
2025-06-13 17:40:08 -0500

    add: app logic

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { CreateToken } from "./components/CreateToken";
+import backdrop from "./public/backdrop.webp";
+
+function App() {
+  return (
+    <div
+      className="p-5 min-h-screen "
+      style={{
+        backgroundImage: `url(${backdrop})`,
+        backgroundOrigin: "border-box",
+        backgroundSize: "100vw",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <ConnectionProvider endpoint="https://api.devnet.solana.com">
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div className="flex flex-col items-end gap-3 absolute right-4 ">
+              <WalletMultiButton />
+            </div>
+            <CreateToken />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
+}
+
+export default App;
