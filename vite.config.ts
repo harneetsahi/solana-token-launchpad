ab3a5f09940663d18373a5c8b2f67aee3ec42cea
Harneet <harneetsahi8@gmail.com>
2025-06-13 17:37:59 -0500

    update: better types integration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), nodePolyfills()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      include: ["stream", "buffer", "process"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
 });
