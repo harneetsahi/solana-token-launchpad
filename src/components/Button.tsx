a4971edaaac8fc1f299d05a7d5a5dce0c36dddb5
Harneet <harneetsahi8@gmail.com>
2025-06-13 19:57:30 -0500

    update: icon option

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,12 @@
+import { ReactNode } from "react";
+
 interface IButton {
   onClick?: () => void;
   text: string;
   className?: string;
   type?: "button" | "submit";
   disabled?: boolean;
+  icon?: ReactNode;
 }
 
 export const Button = ({
@@ -12,16 +15,18 @@ export const Button = ({
   className,
   type,
   disabled,
+  icon,
 }: IButton) => {
   return (
     <button
-      className={`px-6 py-3.5 text-[#0A0E13] bg-[#C7F283] font-medium rounded-xl  ${className} ${
+      className={`px-6 py-3.5 text-[#0A0E13] bg-[#C7F283] font-medium rounded-xl flex justify-center items-center ${className} ${
         !disabled && "cursor-pointer"
       }`}
       type={type}
       onClick={onClick}
       disabled={disabled}
     >
+      {icon}
       {text}
     </button>
   );
